33aa0035bacf33d67edc66e5e460d4f7af238116
Mats Lindh <mats@lindh.no>
2015-09-04 20:53:31 +0200

    Remove references to CHANGES.txt and the file itself

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
-
 requires = [
     'pyramid',
     'pyramid_chameleon',
