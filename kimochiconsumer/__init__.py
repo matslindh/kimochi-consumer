4b018935c4729aff0dfcff709331f840dd05e8b6
Mats Lindh <mats@lindh.no>
2015-09-13 14:25:28 +0200

    Add kimochi client to request to make it available to views

--- kimochiconsumer/__init__.py
+++ kimochiconsumer/__init__.py
@@ -1,12 +1,19 @@
 from pyramid.config import Configurator
+import kimochiconsumer.kimochi
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    def get_kimochi(request):
+        return kimochi.Kimochi(settings['kimochi.url'],
+                               settings['kimochi.api_key'],
+                               site_key=settings['kimochi.site_key'])
+
     config = Configurator(settings=settings)
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('page', '/')
+    config.add_request_method(get_kimochi, 'kimochi', reify=True)
     config.scan()
     return config.make_wsgi_app()
